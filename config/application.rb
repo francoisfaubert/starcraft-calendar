6ddd5c021c380bc0763cd4de88c8eed6303bd633
Francois Faubert <frank@francoisfaubert.com>
2014-10-29 15:36:41 -0400

    postgres wont load

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module StarcraftCalendar
   class Application < Rails::Application
