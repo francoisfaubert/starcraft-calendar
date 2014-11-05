8a0fc4b453f5ad0c42d87c430481ed634fff4439
Francois Faubert <frank@francoisfaubert.com>
2014-11-05 19:27:50 +0000

    added support for remote requests

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,13 @@
 class ApiController < ApplicationController
+
+    after_filter :cors_set_access_control_headers
+
+    def cors_set_access_control_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'GET'
+      headers['Access-Control-Max-Age'] = "1728000"
+    end
+
     def events
         render :json => Wcs.limit(30).order('start')
     end
